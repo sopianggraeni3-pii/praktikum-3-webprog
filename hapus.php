a2b4182d670993ffe1bf87dd3d2fb720f8350b31
sopi <24700611121@student.unsil.ac.id>
2026-02-25 22:30:47 +0700

    init

---
+++ hapus.php
@@ -0,0 +1,9 @@
+<?php
+require 'connection.php';
+$id = $_GET['id'];
+$sql = "DELETE FROM mahasiswa WHERE id = ?";
+$stmt = $pdo->prepare($sql);
+if ($stmt->execute([$id])) {
+    header("Location: index.php");
+    exit;
+}
